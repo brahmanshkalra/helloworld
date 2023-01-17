87f86ff0a783e845c8679f25321505d484acfee5
Brahmansh Kalra <Bkalra2@myseneca.ca>
2023-01-17 14:57:07 +0300

    update include studen name, id and teh app link

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
+/*********************************************************************************
+* WEB322 – Assignment 1
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy.
+* No part of this assignment has been copied manually or electronically from any other source
+* (including web sites) or distributed to other students.
+*
+* Name: Brahmansh Kalra     Student ID: 155401219  Date: 17/01/2023
+*
+* Online (Cyclic) URL:https://cautious-slippers-crow.cyclic.app/
+*
+********************************************************************************/ 
+
 const express =  require("express");
 var http_port = process.env.PORT || 8000;
 var app =  express();
 
 app.get("/", (reg, res) =>{
-    res.send("<h1>Hello world<h1>");
+    res.send("<h1>Brahmansh Kalra - 155401219<h1>");
 });
 
 app.listen(http_port);
